d1fb6a2de9f66fea176f10eb88211422d315af1b
RobertKim <robertjoonkim@gmail.com>
2021-01-06 19:00:30 -0800

    refactored for clarity/readability

--- lacroix_survey.rb
+++ lacroix_survey.rb
@@ -1,7 +1,5 @@
-require 'tempfile'
-
 class SurveyParser
-	attr_accessor :in_file_path
+	attr_accessor :file_path
 
 	RANK_TO_POINTS = {
 		1 => 5,
@@ -12,11 +10,11 @@ class SurveyParser
 	}
 
 	def initialize(args)
-		@in_file_path = args
+		@file_path = args
 	end
 
 	def run
-		result = formatted_points_array.join("\n")
+		result = ranked_flavors
 		puts result
 		result
 	end
@@ -24,64 +22,44 @@ class SurveyParser
 	private
 
 	def file_data
-		file_data = in_file_path.first
-		@raw_file_data ||= File.readlines(file_data)
+		raw_file_data = file_path.first
+		@file_data ||= File.readlines(raw_file_data)
 	end
 
-	def file_data_arrays(data=file_data)
-		file_data.map{|x| x.split(" ")}
-	end
-
-	# each product word with rank in array, nested
-	def grouped_data_arrays
-		file_data_arrays.map{|x| [x[0..-2], x[-1]]}
-		
-	end
-
-	# full product name with rank, nested
-	def product_with_rank_arrays
+	def surveys_with_ranks
+		file_data_arrays = file_data.map{|x| x.split(" ")}
+		grouped_data_arrays = file_data_arrays.map{|x| [x[0..-2], x[-1]]}
 		grouped_data_arrays.map{|x| [x[0].join(" "), x[-1].to_i]}
 	end
 
-	# full product name with converted point value, nested
-	def product_with_points_arrays
-		product_with_rank_arrays.each do |x|
+	# full flavor name with converted point value, nested
+	def surveys_with_points
+		surveys_with_ranks.each do |x|
 			points = RANK_TO_POINTS[x.last]
 			x.pop
 			x.push(points)
 		end
 	end
 
-	# hash of product to totaled points
-	def points_hash
-		points_hash = Hash.new(0)
-		product_with_points_arrays.each do |x|
-			points_hash[x.first] += x.last
+	# hash of flavor to totaled points
+	def totaled_points
+		totaled_points = Hash.new(0)
+		surveys_with_points.each do |x|
+			totaled_points[x.first] += x.last
 		end
-		points_hash
+		totaled_points
 	end
 	
-	def sorted_points_hash
-		points_hash.sort_by{|k,v| [-v, k.length]}
+	def ranked_flavors
+		sorted_array = totaled_points.sort_by{|k,v| [-v, k.length]}
+		sorted_array.each.with_index do |x,i|
+			sorted_array[i].last == sorted_array[i-1].last ? x.unshift(i) : x.unshift(i+1)
+		end		
+		result = sorted_array.map{|x|"#{x[0]}. #{x[1]}, #{x[-1]} pts "}.join("\n")
+		puts result
+		result
 	end
 
-	def points_array
-		sorted_points_hash.to_a
-	end
-
-	# Formatting for readability
-	def formatted_points_array
-		formatted_points_array = []
-		points_array.each.with_index do |x,i|
-			formatted_line_array = []			
-			formatted_line_array << "#{(i+1)}. #{x.first}"
-			formatted_line_array << "#{x.last} pts "
-			formatted_points_array << formatted_line_array.join(", ")
-		end	
-		p formatted_points_array
-		formatted_points_array
-	end
-	
 end
 
 if $PROGRAM_NAME == __FILE__
