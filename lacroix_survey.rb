a3800ca8bac693eddd54ac1c46415596c8d86f9c
RobertKim <robertjoonkim@gmail.com>
2021-01-06 17:15:41 -0800

    WIP: bug from tie in rankings / committing working rspec

--- lacroix_survey.rb
+++ lacroix_survey.rb
@@ -1,46 +1,89 @@
-filename = ARGV
-file = filename.first
+require 'tempfile'
 
-ary = File.readlines(file).map{|x| x.split(" ")}
+class SurveyParser
+	attr_accessor :in_file_path
 
-new_ary = ary.map{|x| [x[0..-2], x[-1]]}
+	RANK_TO_POINTS = {
+		1 => 5,
+		2 => 3,
+		3 => 2,
+		4 => 1,
+		5 => 0
+	}
 
-ary_of_arys = new_ary.map{|x| [x[0].join(" "), x[-1].to_i]}
-# [["Guava Java", 1], ["Grape Escape", 2], ["Star Fruit Salute", 3], ["Blackberry Fairy", 4], ["Banana Cabana", 5], ["Banana Cabana", 1], ["Star Fruit Salute", 2], ["Grape Escape", 3], ["Guava Java", 4], ["Blackberry Fairy", 5], ["Banana Cabana", 1], ["Grape Escape", 2], ["Star Fruit Salute", 3], ["Guava Java", 4], ["Blackberry Fairy", 5]]
+	def initialize(args)
+		@in_file_path = args
+	end
 
-# puts yo
+	def run
+		result = formatted_points_array.join("\n")
+		puts result
+		result
+	end
 
-RANK_TO_POINTS = {
-	1 => 5,
-	2 => 3,
-	3 => 2,
-	4 => 1,
-	5 => 0
-}
+	private
 
-ary_of_arys.each do |x|
-	points = RANK_TO_POINTS[x.last]
-	x.pop
-	x.push(points)
+	def file_data
+		file_data = in_file_path.first
+		@raw_file_data ||= File.readlines(file_data)
+	end
+
+	def file_data_arrays(data=file_data)
+		file_data.map{|x| x.split(" ")}
+	end
+
+	# each product word with rank in array, nested
+	def grouped_data_arrays
+		file_data_arrays.map{|x| [x[0..-2], x[-1]]}
+		
+	end
+
+	# full product name with rank, nested
+	def product_with_rank_arrays
+		grouped_data_arrays.map{|x| [x[0].join(" "), x[-1].to_i]}
+	end
+
+	# full product name with converted point value, nested
+	def product_with_points_arrays
+		product_with_rank_arrays.each do |x|
+			points = RANK_TO_POINTS[x.last]
+			x.pop
+			x.push(points)
+		end
+	end
+
+	# hash of product to totaled points
+	def points_hash
+		points_hash = Hash.new(0)
+		product_with_points_arrays.each do |x|
+			points_hash[x.first] += x.last
+		end
+		points_hash
+	end
+	
+	def sorted_points_hash
+		points_hash.sort_by{|k,v| [-v, k.length]}
+	end
+
+	def points_array
+		sorted_points_hash.to_a
+	end
+
+	# Formatting for readability
+	def formatted_points_array
+		formatted_points_array = []
+		points_array.each.with_index do |x,i|
+			formatted_line_array = []			
+			formatted_line_array << "#{(i+1)}. #{x.first}"
+			formatted_line_array << "#{x.last} pts "
+			formatted_points_array << formatted_line_array.join(", ")
+		end	
+		p formatted_points_array
+		formatted_points_array
+	end
+	
 end
 
-points_hash = Hash.new(0)
-ary_of_arys.each do |x|
-	points_hash[x.first] += x.last
+if $PROGRAM_NAME == __FILE__
+  SurveyParser.new(ARGV).run
 end
-
-ordered_points_hash = points_hash.sort_by{|k,v| [-v, k.length]}
-
-points_ary = ordered_points_hash.to_a
-
-# Formatting for readability
-formatted_points_ary = []
-points_ary.each.with_index do |x,i|
-	formatted = []
-	formatted << (i+1).to_s + ". " + x.first
-	formatted << x.last.to_s + " pts"
-	formatted_points_ary << formatted.join(", ")
-end
-
-puts formatted_points_ary.join("\n")
-# p formatted_points_ary
---
+++ lacroix_survey_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require 'pry'
+
+require_relative './lacroix_survey.rb'
+
+RSpec.describe SurveyParser do
+  describe '#run' do
+	  context 'with sample input provided' do
+	  	let(:arguments) { ["sample-input.txt"] }
+			let(:buff) { SurveyParser.new(arguments) }
+
+		  it "outputs the points for each drink in the survey results" do
+		    expect(buff.run).to eq(
+		    	"1. Banana Cabana, 10 pts \n2. Grape Escape, 8 pts \n3. Guava Java, 7 pts \n3. Star Fruit Salute, 7 pts \n5. Blackberry Fairy, 1 pts "
+		    )
+  		end
+  	end
+  end
+end
